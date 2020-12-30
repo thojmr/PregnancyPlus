8aa975833758ee0c0e0b00e5f485164164b4400e
thojm <thojmr@gmail.com>
2020-12-30 14:51:07 -0500

    Added release package script

---
+++ release.cmd
@@ -0,0 +1,7 @@
+::Zips the dll into the correct directory structure for release
+::Make sure to increment the version
+
+::CLear last release
+rd /s /q "./release"
+
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip "./release/KK_PregnancyPlus v0.2.zip" "./bin/BepinEx" -mx0
