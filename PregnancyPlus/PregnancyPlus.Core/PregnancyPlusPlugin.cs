f09585df6c788a49baf33a7b024834d3ed584af0
thojm <thojmr@gmail.com>
2021-01-14 18:10:18 -0500

    increment version

--- PregnancyPlus/PregnancyPlus.Core/PregnancyPlusPlugin.cs
+++ PregnancyPlus/PregnancyPlus.Core/PregnancyPlusPlugin.cs
@@ -19,7 +19,7 @@ namespace KK_PregnancyPlus
     public partial class PregnancyPlusPlugin : BaseUnityPlugin
     {
         public const string GUID = "KK_PregnancyPlus";
-        public const string Version = "1.0";
+        public const string Version = "1.1";
         internal static new ManualLogSource Logger { get; private set; }
 
         #if DEBUG
--- release.cmd
+++ release.cmd
@@ -1,7 +1,7 @@
 ::Zips the dll into the correct directory structure for release
 ::Make sure to increment the version
 
-set kk_version=1.0
+set kk_version=1.1
 set kk_name=KK_PregnancyPlus
 set hs2_name=HS2_PregnancyPlus
 set ai_name=AI_PregnancyPlus
@@ -9,3 +9,5 @@ set ai_name=AI_PregnancyPlus
 IF EXIST "./bin/%kk_name%/BepinEx/plugins/%kk_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%kk_name% v%kk_version%.zip" "./bin/%kk_name%/BepinEx" -mx0
 IF EXIST "./bin/%hs2_name%/BepinEx/plugins/%hs2_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%hs2_name% v%kk_version%.zip" "./bin/%hs2_name%/BepinEx" -mx0
 IF EXIST "./bin/%ai_name%/BepinEx/plugins/%ai_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%ai_name% v%kk_version%.zip" "./bin/%ai_name%/BepinEx" -mx0
+
+start %HOMEPATH%/downloads
