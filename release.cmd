b67615da04a4eb7106aa7738fe03bd4da903c3ad
thojm <thojmr@gmail.com>
2021-02-03 21:51:32 -0500

    version increment

--- release.cmd
+++ release.cmd
@@ -1,7 +1,7 @@
 ::Zips the dll into the correct directory structure for release
 ::Make sure to increment the version
 
-set kk_version=1.16
+set kk_version=1.17
 set kk_name=KK_PregnancyPlus
 set hs2_name=HS2_PregnancyPlus
 set ai_name=AI_PregnancyPlus
