1b61c27dda3e6b5cc0397dec8cbeb93ec752d2db
thojm <thojmr@gmail.com>
2021-02-14 17:06:46 -0500

    increment version 1.22

--- PregnancyPlus/PregnancyPlus.Core/PPPlugin.cs
+++ PregnancyPlus/PregnancyPlus.Core/PPPlugin.cs
@@ -21,7 +21,7 @@ namespace KK_PregnancyPlus
     public partial class PregnancyPlusPlugin : BaseUnityPlugin
     {
         public const string GUID = "KK_PregnancyPlus";
-        public const string Version = "1.21";
+        public const string Version = "1.22";
         internal static new ManualLogSource Logger { get; private set; }
 
         #if DEBUG
