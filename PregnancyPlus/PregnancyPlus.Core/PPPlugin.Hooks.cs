dbb539ed6509294de36b7eaf3589f81fe592bd81
Crescent696 <crescent696@icloud.com>
2021-06-23 04:43:59 +0800

    Add cumflation support in multiplay mode.

--- PregnancyPlus/PregnancyPlus.Core/PPPlugin.Hooks.cs
+++ PregnancyPlus/PregnancyPlus.Core/PPPlugin.Hooks.cs
@@ -2,6 +2,7 @@
 using KKAPI.Maker;
 using KKAPI.Studio;
 using KKAPI.Chara;
+using System.Linq;
 #if AI || HS2
     using AIChara;
 #elif KK
@@ -30,59 +31,127 @@ namespace KK_PregnancyPlus
             {
                 harmonyInstance.PatchAll(typeof(Hooks));
             }
-			
-			#if HS2
+
+            #if HS2
+
+                //MultiPlay_F2M1
+                [HarmonyPrefix]
+                [HarmonyPatch(typeof(MultiPlay_F2M1), "Proc", typeof(int), typeof(HScene.AnimationListInfo))]
+                public static void MultiPlay_F2M1_Proc(MultiPlay_F2M1 __instance, int _modeCtrl, HScene.AnimationListInfo _infoAnimList)
+                {
+                    //Get current user button click type
+                    var ctrlFlag = Traverse.Create(__instance).Field("ctrlFlag").GetValue<HSceneFlagCtrl>();
+                    DetermineInflationState(ctrlFlag, _infoAnimList, "MultiPlay_F2M1_Proc");
+                }
+
+                //Multiplay F1M2
+                [HarmonyPrefix]
+                [HarmonyPatch(typeof(MultiPlay_F1M2), "Proc", typeof(int), typeof(HScene.AnimationListInfo))]
+                public static void MultiPlay_F1M2_Proc(MultiPlay_F1M2 __instance, int _modeCtrl, HScene.AnimationListInfo _infoAnimList)
+                {
+                    //Get current user button click type
+                    var ctrlFlag = Traverse.Create(__instance).Field("ctrlFlag").GetValue<HSceneFlagCtrl>();
+                    DetermineInflationState(ctrlFlag, _infoAnimList, "MultiPlay_F1M2_Proc");
+                }
 
                 //HS2 inflation trigger logic, copied from KK_Pregnancy
                 [HarmonyPrefix]
                 [HarmonyPatch(typeof(Sonyu), "Proc", typeof(int), typeof(HScene.AnimationListInfo))]
-                public static void Sonyu_Proc(Sonyu __instance)
-                {                    
+                public static void Sonyu_Proc(Sonyu __instance, int _modeCtrl, HScene.AnimationListInfo _infoAnimList)
+                {
                     //Get current user button click type
                     var ctrlFlag = Traverse.Create(__instance).Field("ctrlFlag").GetValue<HSceneFlagCtrl>();                                    
-                    DetermineInflationState(ctrlFlag);                   
+                    DetermineInflationState(ctrlFlag, _infoAnimList, "Sonyu_Proc");      
                 }
 
 
                 [HarmonyPrefix]
                 [HarmonyPatch(typeof(Houshi), "Proc", typeof(int), typeof(HScene.AnimationListInfo))]
-                public static void Houshi_Proc(Houshi __instance)
-                {                    
+                public static void Houshi_Proc(Houshi __instance, int _modeCtrl, HScene.AnimationListInfo _infoAnimList)
+                {
                     //Get current user button click type
-                    var ctrlFlag = Traverse.Create(__instance).Field("ctrlFlag").GetValue<HSceneFlagCtrl>(); 
-                    DetermineInflationState(ctrlFlag);                                   
+                    var ctrlFlag = Traverse.Create(__instance).Field("ctrlFlag").GetValue<HSceneFlagCtrl>();                
+                    DetermineInflationState(ctrlFlag, _infoAnimList, "Houshi_Proc");                                   
                 }
 
                 //When user clicks finish button, set the inflation based on the button clicked
-                private static void DetermineInflationState(HSceneFlagCtrl ctrlFlag)
+                private static void DetermineInflationState(HSceneFlagCtrl ctrlFlag, HScene.AnimationListInfo _infoAnimList,string source=null)
                 {
-                    //swallow clicked
                     if (ctrlFlag.click == HSceneFlagCtrl.ClickKind.FinishInSide 
                         || ctrlFlag.click == HSceneFlagCtrl.ClickKind.FinishSame  
                         || ctrlFlag.click == HSceneFlagCtrl.ClickKind.FinishDrink
                         || ctrlFlag.click == HSceneFlagCtrl.ClickKind.FinishVomit) 
                     {
-                        TriggerInflation();                   
-                    }            
+                        if (PregnancyPlusPlugin.DebugLog.Value) PregnancyPlusPlugin.Logger.LogInfo($"DetermineInflationState {ctrlFlag.click.ToString()} from {source}");
+
+                        var InflationCharaTargetCtrl = DetermineInflationTarget(ctrlFlag, _infoAnimList);
+
+                        if (InflationCharaTargetCtrl == null) 
+                            PregnancyPlusPlugin.Logger.LogWarning($"Cant determine which female to inflate");
+                        else
+                            TriggerInflation(InflationCharaTargetCtrl);
+                    }          
                 }
+
+
 
                 /// <summary>
                 /// Add for cumflation effect in HS2 only.  (Too lazy to implement deflation logic right now)
                 /// </summary>
-                private static void TriggerInflation(bool deflate = false)
+                private static void TriggerInflation(PregnancyPlusCharaController charCustFunCtrl, bool deflate = false)
                 {
+                    if (charCustFunCtrl == null) return;
                     if (StudioAPI.InsideStudio || MakerAPI.InsideMaker) return;//Don't allow in studio/maker
                     if (!StoryMode.Value || !AllowCumflation.Value) return;
                                         
-                    if (PregnancyPlusPlugin.DebugLog.Value) PregnancyPlusPlugin.Logger.LogInfo($" HSceneFlagCtrl_AddOrgasm_HS2");
-                    var handlers = CharacterApi.GetRegisteredBehaviour(GUID);
+                    if (PregnancyPlusPlugin.DebugLog.Value) PregnancyPlusPlugin.Logger.LogInfo($"TriggerInflation to {charCustFunCtrl.name}:{charCustFunCtrl.charaFileName}");
 
-                    //This will probably inflate all characters in scene, but I don't care
-                    foreach (PregnancyPlusCharaController charCustFunCtrl in handlers.Instances)
+                    charCustFunCtrl.HS2Inflation();
+                }
+
+                /// <summary>
+                /// determine which female to inflate in HS2 includs multiplay mode.
+                /// Check value _infoAnimList.fileFemale, 2rd value count from bottom up is what we need.
+                /// example:h2_mf2_f1_01
+                ///                 ^This value indicates which female is inserted.
+                /// </summary>      
+                private static PregnancyPlusCharaController DetermineInflationTarget(HSceneFlagCtrl ctrlFlag, HScene.AnimationListInfo _infoAnimList)
+                {
+                    var CharaControllers = CharacterApi.GetRegisteredBehaviour(GUID).Instances;
+                    PregnancyPlusCharaController InflationCharaTarget = null;
+
+                    if (string.IsNullOrEmpty(_infoAnimList.fileFemale2) == false)
                     {
-                        //Trigger inflation
-                        charCustFunCtrl.HS2Inflation(deflate);
-                    }                    
+                        if (PregnancyPlusPlugin.DebugLog.Value) PregnancyPlusPlugin.Logger.LogInfo($"HScene is multiplay F2M1");
+
+                        if (_infoAnimList.fileFemale == "h2_mf2_f1_01" || _infoAnimList.fileFemale == "h2_mf2_f2_04")
+                        {
+                            if (PregnancyPlusPlugin.DebugLog.Value) PregnancyPlusPlugin.Logger.LogInfo($"Ignore inflation when sumata/intercrural sex.");
+                        }
+                        else
+                        {
+                        
+                            var fileFemaleSplit = _infoAnimList.fileFemale.Split('_').Reverse().ToArray();
+                            if (fileFemaleSplit.Length < 3)
+                                PregnancyPlusPlugin.Logger.LogWarning($"bad _infoAnimList.fileFemale:{_infoAnimList.fileFemale}");
+                            else
+                            {
+                                if (fileFemaleSplit[1] == "f1")
+                                    InflationCharaTarget = CharaControllers.FirstOrDefault(x => x.name == "chaF_001") as PregnancyPlusCharaController;
+                                else if (fileFemaleSplit[1] == "f2")
+                                    InflationCharaTarget = CharaControllers.FirstOrDefault(x => x.name == "chaF_002") as PregnancyPlusCharaController;
+                                else
+                                    PregnancyPlusPlugin.Logger.LogWarning($"bad fileFemaleSplit[1]:{fileFemaleSplit[1]}");
+                            }
+                        }
+                    }
+                    else // Normal mode
+                    {
+                        InflationCharaTarget = CharaControllers.FirstOrDefault(x => x.name == "chaF_001") as PregnancyPlusCharaController;
+                    }
+
+
+                    return InflationCharaTarget;
                 }
             #endif
 
