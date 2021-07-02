73aa4ac2e8d9ecae59508c267bc216ef15e7312f
Crescent696 <crescent696@icloud.com>
2021-07-02 15:52:00 +0800

    Modify HS2Inflation for inflate and deflate control.

--- PregnancyPlus/PregnancyPlus.Core/PPCharaController.HS2_Inflation.cs
+++ PregnancyPlus/PregnancyPlus.Core/PPCharaController.HS2_Inflation.cs
@@ -17,26 +17,32 @@ namespace KK_PregnancyPlus
     //This partial class contains the logic for HS2 belly infaltion
     public partial class PregnancyPlusCharaController: CharaCustomFunctionController
     {   
-
         //How many times the belly has been inflated, up to he max
         public int currentInflationLevel = 0;
-        const int maxInflationLevel = 6;
+        public int maxInflationLevel = 6;
+
+        public int inflationStep = 1;
+        public int deflationStep = 1;
 
 
         /// <summary>
         /// Trigger belly inflation in HS2 after hsceneflag hook fired
         /// </summary>
         public void HS2Inflation(bool deflate = false)
-        {                
-            if (deflate) currentInflationLevel = 0;
-            if (currentInflationLevel < maxInflationLevel) 
-            {
-                //Increment inflation count
-                currentInflationLevel += 1;
-            }
+        {
+            int _nextInflationLevel;
+
+            if (deflate)
+                _nextInflationLevel = currentInflationLevel - deflationStep;
+            else
+                _nextInflationLevel = currentInflationLevel + inflationStep;
+            
+            //clip 
+            currentInflationLevel = Math.Max(0, Math.Min(_nextInflationLevel, maxInflationLevel));
                   
             //Re-use the kk pregnancy inflation code here to smooth the inflation animation
             OnInflationChanged(currentInflationLevel, maxInflationLevel);                    
         }
+
     }
 }
