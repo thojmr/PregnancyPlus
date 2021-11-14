3769d898f7a49fe16c5a17fa1a1c5e59e87b0302
thojm <thojmr@gmail.com>
2021-11-14 16:20:17 -0500

    increment version 5.5

--- PregnancyPlus/PregnancyPlus.Core/PPPlugin.cs
+++ PregnancyPlus/PregnancyPlus.Core/PPPlugin.cs
@@ -29,7 +29,7 @@ namespace KK_PregnancyPlus
     public partial class PregnancyPlusPlugin : BaseUnityPlugin
     {
         public const string GUID = "KK_PregnancyPlus";
-        public const string Version = "5.4";
+        public const string Version = "5.5";
         internal static new ManualLogSource Logger { get; private set; }
    
 
