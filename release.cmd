affa30712383bb102529d670d3e83a9bc34b248d
thojm <thojmr@gmail.com>
2022-01-18 14:22:50 -0500

    Fixed release script not working from remote drive

--- release.cmd
+++ release.cmd
@@ -7,9 +7,9 @@ set kks_name=KKS_PregnancyPlus
 set hs2_name=HS2_PregnancyPlus
 set ai_name=AI_PregnancyPlus
 
-IF EXIST "./bin/%kk_name%/BepinEx/plugins/%kk_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%kk_name% v%kk_version%.zip" "./bin/%kk_name%/BepinEx" -mx0
-IF EXIST "./bin/%kks_name%/BepinEx/plugins/%kks_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%kks_name% v%kk_version%.zip" "./bin/%kks_name%/BepinEx" -mx0
-IF EXIST "./bin/%hs2_name%/BepinEx/plugins/%hs2_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%hs2_name% v%kk_version%.zip" "./bin/%hs2_name%/BepinEx" -mx0
-IF EXIST "./bin/%ai_name%/BepinEx/plugins/%ai_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEPATH%/downloads/%ai_name% v%kk_version%.zip" "./bin/%ai_name%/BepinEx" -mx0
+IF EXIST "./bin/%kk_name%/BepinEx/plugins/%kk_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEDRIVE%%HOMEPATH%/downloads/%kk_name% v%kk_version%.zip" "./bin/%kk_name%/BepinEx" -mx0
+IF EXIST "./bin/%kks_name%/BepinEx/plugins/%kks_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEDRIVE%%HOMEPATH%/downloads/%kks_name% v%kk_version%.zip" "./bin/%kks_name%/BepinEx" -mx0
+IF EXIST "./bin/%hs2_name%/BepinEx/plugins/%hs2_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEDRIVE%%HOMEPATH%/downloads/%hs2_name% v%kk_version%.zip" "./bin/%hs2_name%/BepinEx" -mx0
+IF EXIST "./bin/%ai_name%/BepinEx/plugins/%ai_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEDRIVE%%HOMEPATH%/downloads/%ai_name% v%kk_version%.zip" "./bin/%ai_name%/BepinEx" -mx0
 
-start %HOMEPATH%/downloads
+start %HOMEDRIVE%%HOMEPATH%/downloads
