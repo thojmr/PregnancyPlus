8552ad05f97fd619e194bbf9de7fba08edf5b327
thojm <thojmr@gmail.com>
2022-01-27 16:39:06 -0500

    increment version 6.1

--- PregnancyPlus/PregnancyPlus.Core/PPPlugin.cs
+++ PregnancyPlus/PregnancyPlus.Core/PPPlugin.cs
@@ -32,7 +32,7 @@ namespace KK_PregnancyPlus
     public partial class PregnancyPlusPlugin : BaseUnityPlugin
     {
         public const string GUID = "KK_PregnancyPlus";
-        public const string Version = "6.0";
+        public const string Version = "6.1";
         internal static new ManualLogSource Logger { get; private set; }
    
 
--- release.cmd
+++ release.cmd
@@ -1,7 +1,7 @@
 ::Zips the dll into the correct directory structure for release
 ::Make sure to increment the version
 
-set kk_version=6.0
+set kk_version=6.1
 set kk_name=KK_PregnancyPlus
 set kks_name=KKS_PregnancyPlus
 set hs2_name=HS2_PregnancyPlus
