9ffee2663ad0579e01f0f79c69f86e10299ffb1f
thojm <thojmr@gmail.com>
2022-02-19 13:13:35 -0500

    increment version 6.3

--- release.cmd
+++ release.cmd
@@ -1,7 +1,7 @@
 ::Zips the dll into the correct directory structure for release
 ::Make sure to increment the version
 
-set kk_version=6.2
+set kk_version=6.3
 set kk_name=KK_PregnancyPlus
 
 IF EXIST "./bin/%kk_name%/BepinEx/plugins/%kk_name%.dll" "%ProgramFiles%\7-Zip\7z.exe" a -tzip "%HOMEDRIVE%%HOMEPATH%/downloads/%kk_name% v%kk_version%.zip" "./bin/%kk_name%/BepinEx" -mx0
